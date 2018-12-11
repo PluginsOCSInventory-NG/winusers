b65baa2e7e69eff439ceddc0f9407fdf843fe851
Charlene Auger <charlene.auger@factorfx.com>
2018-12-11 11:33:28 +0100

    Update plugin for extension engine

--- install.php
+++ install.php
@@ -1,40 +1,39 @@
 <?php
-function plugin_version_winusers()
+
+/**
+ * This function is called on installation and is used to create database schema for the plugin
+ */
+function extension_install_winusers()
 {
-return array('name' => 'winusers',
-'version' => '1.1',
-'author'=> 'Guillaume PRIOU, Gilles DUBOIS',
-'license' => 'GPLv2',
-'verMinOcs' => '2.2');
+    $commonObject = new ExtensionCommon;
+
+    $commonObject -> sqlQuery("CREATE TABLE IF NOT EXISTS `winusers` (
+                             `ID` INT(11) NOT NULL AUTO_INCREMENT,
+                             `HARDWARE_ID` INT(11) NOT NULL,
+                             `NAME` VARCHAR(255) DEFAULT NULL,
+                             `TYPE` VARCHAR(255) DEFAULT NULL,
+                             `SIZE` VARCHAR(255) DEFAULT NULL,
+                             `LASTLOGON` VARCHAR(255) DEFAULT NULL,
+                             `DESCRIPTION` VARCHAR(255) DEFAULT NULL,
+                             `STATUS` VARCHAR(255) DEFAULT NULL,
+                             `SID` VARCHAR(255) DEFAULT NULL,
+                             PRIMARY KEY  (`ID`,`HARDWARE_ID`)
+                             ) ENGINE=InnoDB ;");
 }
 
-function plugin_init_winusers()
+/**
+ * This function is called on removal and is used to destroy database schema for the plugin
+ */
+function extension_delete_winusers()
 {
-$object = new plugins;
-$object -> add_cd_entry("winusers","other");
-
-$object -> sql_query("CREATE TABLE IF NOT EXISTS `winusers` (
- `ID` INT(11) NOT NULL AUTO_INCREMENT,
- `HARDWARE_ID` INT(11) NOT NULL,
- `NAME` VARCHAR(255) DEFAULT NULL,
- `TYPE` VARCHAR(255) DEFAULT NULL,
- `SIZE` VARCHAR(255) DEFAULT NULL,
- `LASTLOGON` VARCHAR(255) DEFAULT NULL,
- `DESCRIPTION` VARCHAR(255) DEFAULT NULL,
- `STATUS` VARCHAR(255) DEFAULT NULL,
- `SID` VARCHAR(255) DEFAULT NULL,
- PRIMARY KEY  (`ID`,`HARDWARE_ID`)
- ) ENGINE=InnoDB ;");
-
+    $commonObject = new ExtensionCommon;
+    $commonObject -> sqlQuery("DROP TABLE `winusers`;");
 }
 
-function plugin_delete_winusers()
+/**
+ * This function is called on plugin upgrade
+ */
+function extension_upgrade_winusers()
 {
-$object = new plugins;
-$object -> del_cd_entry("winusers");
-
-$object -> sql_query("DROP TABLE `winusers`;");
 
 }
-
-?>
